f19bbcbc58547c16d66fc9a0a5a2cfc9e1e4119d
Obiora Okwara <ObioraOkwara10@gmail.com>
2026-06-21 00:29:02 +0100

    Welcome to WeCare

---
+++ main.js
@@ -0,0 +1,45 @@
+// make the link button dynamics
+//make the emergency arrow button dynamic
+//animate the webpage
+
+//try to make hamburger button work with javascript
+
+const button = document.querySelector(".hamburger");
+const navBox = document.querySelector(".nav");
+const box = document.querySelector(".box")
+
+navBox.classList.add('hidden');
+
+button.addEventListener("click", function() {
+    // Toggles the class on/off every click
+    button.classList.toggle('open');
+    navBox.classList.toggle('hidden');
+});
+
+const c1 = document.getElementById("c-1");
+const c2 = document.getElementById("c-2");
+const c3 = document.getElementById("c-3");
+const c4 = document.getElementById("c-4");
+const c5 = document.getElementById("c-5");
+const c6 = document.getElementById("c-6");
+const c7 = document.getElementById("c-7");
+const c8 = document.getElementById("c-8");
+const c9 = document.getElementById("c-9");
+const c10 = document.getElementById("c-10");
+const c11 = document.getElementById("c-11");
+const c12 = document.getElementById("c-12");
+
+const carousel = document.getElementsByClassName("carousel-contr");
+
+c7.innerHTML = c1.innerHTML;
+c8.innerHTML = c2.innerHTML;
+c9.innerHTML = c3.innerHTML;
+c10.innerHTML = c4.innerHTML;
+c11.innerHTML = c5.innerHTML;
+c12.innerHTML = c6.innerHTML;
+
+const observer = new MutationObsever((mutations) => {
+    console.log("A comment was updated")
+});
+
+observer.observe(carousel, { childList: true, characterData: true });
